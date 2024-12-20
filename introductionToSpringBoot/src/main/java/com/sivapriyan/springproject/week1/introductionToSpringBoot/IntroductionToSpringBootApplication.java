b4d37d572a25dfb39dbeb502a42f396c47310a58
Sivapriyan07 <sivapriyan7siva@gmail.com>
2024-12-20 09:50:42 +0530

    Springboot

--- introductionToSpringBoot/src/main/java/com/sivapriyan/springproject/week1/introductionToSpringBoot/IntroductionToSpringBootApplication.java
+++ introductionToSpringBoot/src/main/java/com/sivapriyan/springproject/week1/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -1,13 +1,21 @@
 package com.sivapriyan.springproject.week1.introductionToSpringBoot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionToSpringBootApplication {
+public class IntroductionToSpringBootApplication implements CommandLineRunner {
 
+	Apple obj;
 	public static void main(String[] args) {
 		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		obj.eatApple();
+	}
+
 
 }
