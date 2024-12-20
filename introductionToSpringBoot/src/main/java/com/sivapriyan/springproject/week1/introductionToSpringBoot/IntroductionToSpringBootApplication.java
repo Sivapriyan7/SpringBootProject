e4e01e41e8d26a55e45a472061cc0b4a65bb5174
Sivapriyan07 <sivapriyan7siva@gmail.com>
2024-12-20 08:39:28 +0530

    First Commit

---
+++ introductionToSpringBoot/src/main/java/com/sivapriyan/springproject/week1/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sivapriyan.springproject.week1.introductionToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+	}
+
+}
---
+++ introductionToSpringBoot/src/test/java/com/sivapriyan/springproject/week1/introductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivapriyan.springproject.week1.introductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
